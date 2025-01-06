87753c402c6bf924c778bdaf9690300e27e5d0ed
Arnab Biswas <arnab.biswas.gdsc@gmail.com>
2025-01-06 13:15:59 +0530

    Update Online Stock Span.cpp

--- Online Stock Span.cpp
+++ Online Stock Span.cpp
@@ -1,13 +1,22 @@
 class StockSpanner {
- public:
-  int next(int price) {
-    int span = 1;
-    while (!stack.empty() && stack.top().first <= price)
-      span += stack.top().second, stack.pop();
-    stack.emplace(price, span);
-    return span;
-  }
+private:
+stack<pair<int,int>>st;
+public:
+    StockSpanner() {
 
- private:
-  stack<pair<int, int>> stack;  // (price, span)
+    }
+    int next(int price) {
+        if(st.empty()){
+            st.push({price,1});
+            return 1;
+        }
+        int cnt=1;
+        while(!st.empty() && st.top().first<=price){
+            cnt+=st.top().second;
+            st.pop();
+        }
+        st.push({price,cnt});
+        return cnt;
+    }
 };
+
