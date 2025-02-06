9ed5db620b39c28132fc9116f4715c0008957459
Arnab Biswas <arnab.biswas.gdsc@gmail.com>
2025-02-06 13:20:00 +0530

    Update 620. Not Boring Movies.sql

--- SQL 50/620. Not Boring Movies.sql
+++ SQL 50/620. Not Boring Movies.sql
@@ -1,10 +1,6 @@
-SELECT
-  Manager.employee_id,
-  Manager.name,
-  COUNT(Employee.employee_id) AS reports_count,
-  ROUND(AVG(Employee.age)) AS average_age
-FROM Employees AS Manager
-INNER JOIN Employees AS Employee
-  ON (Employee.reports_to = Manager.employee_id)
-GROUP BY 1
-ORDER BY 1;
+SELECT *
+FROM Cinema
+WHERE
+  MOD(id, 2) = 1
+  AND description != 'boring'
+ORDER BY rating DESC;
