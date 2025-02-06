3e6197c2b0484434ad2b7fd5715454c62390c8be
Arnab Biswas <arnab.biswas.gdsc@gmail.com>
2025-02-06 13:54:29 +0530

    Update 1517. Find Users With Valid E-Mails.sql

--- SQL 50/1517. Find Users With Valid E-Mails.sql
+++ SQL 50/1517. Find Users With Valid E-Mails.sql
@@ -1,15 +1,3 @@
-class Solution {
- public:
-  vector<int> findingUsersActiveMinutes(vector<vector<int>>& logs, int k) {
-    vector<int> ans(k);
-    unordered_map<int, unordered_set<int>> idToTimes;
-
-    for (const vector<int>& log : logs)
-      idToTimes[log[0]].insert(log[1]);
-
-    for (const auto& [_, mins] : idToTimes)
-      ++ans[mins.size() - 1];
-
-    return ans;
-  }
-};
+SELECT *
+FROM Users
+WHERE REGEXP_LIKE(mail, '^[A-Za-z]+[A-Za-z0-9\_\.\-]*@leetcode\\.com$');
