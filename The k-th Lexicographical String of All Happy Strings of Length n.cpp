786ba92100aac17b6401b2c06306e1a9550afcf2
Arnab Biswas <arnab.biswas.gdsc@gmail.com>
2025-02-19 06:26:55 +0530

    Update The k-th Lexicographical String of All Happy Strings of Length n.cpp

--- The k-th Lexicographical String of All Happy Strings of Length n.cpp
+++ The k-th Lexicographical String of All Happy Strings of Length n.cpp
@@ -1,22 +1,31 @@
 class Solution {
- public:
-  string getHappyString(int n, int k) {
-    const unordered_map<char, string> nextLetters{
-        {'a', "bc"}, {'b', "ac"}, {'c', "ab"}};
-    queue<string> q{{{"a", "b", "c"}}};
-
-    while (q.front().length() != n) {
-      const string u = q.front();
-      q.pop();
-      for (const char nextLetter : nextLetters.at(u.back()))
-        q.push(u + nextLetter);
+public:
+    string ans="";
+    vector<char> v{'a','b','c'};
+    bool f(int ind,string &s,int &k){
+        if(ind==s.size()){
+            k--;
+            if(k==0){
+                ans=s;
+                return true;
+            }
+            return false;
+        }
+        for(auto &ch:v){
+            if(ind==0 || (ind-1>=0 && s[ind-1]!=ch)){
+                s[ind]=ch;
+                bool dd=f(ind+1,s,k);
+                if(dd) return true;
+                s[ind]='.';
+            }
+        }
+        return false;
     }
 
-    if (q.size() < k)
-      return "";
-
-    for (int i = 0; i < k - 1; ++i)
-      q.pop();
-    return q.front();
-  }
+    string getHappyString(int n, int k) {
+        string s(n,'.');
+        // cout<<s<<endl;
+        f(0,s,k);
+        return ans;
+    }
 };
