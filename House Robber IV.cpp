ca47b744b13046148d6f8b496095071c82b1e2ea
Arnab Biswas <arnab.biswas.gdsc@gmail.com>
2025-03-15 10:46:02 +0530

    Update House Robber IV.cpp

--- House Robber IV.cpp
+++ House Robber IV.cpp
@@ -1,28 +1,37 @@
 class Solution {
- public:
-  int minCapability(vector<int>& nums, int k) {
-    int l = ranges::min(nums);
-    int r = ranges::max(nums);
+    bool isPossible(vector<int> &arr, int k, int maxi) {
+        int n = arr.size();
+        int i = 0;
+        while(i < n) {
+            if(arr[i] <= maxi) {
+                i += 2;
+                k--;
+            } else {
+                i++;
+            }
 
-    while (l < r) {
-      const int m = (l + r) / 2;
-      if (numStolenHouses(nums, m) >= k)
-        r = m;
-      else
-        l = m + 1;
+            if(k == 0) {
+                return true;
+            }
+        }
+        return false;
     }
+public:
+    int minCapability(vector<int>& arr, int k) {
+        int n = arr.size();
+        int low = *min_element(arr.begin(), arr.end());
+        int high = *max_element(arr.begin(), arr.end());
 
-    return l;
-  }
-
- private:
-  int numStolenHouses(const vector<int>& nums, int capacity) {
-    int stolenHouses = 0;
-    for (int i = 0; i < nums.size(); ++i)
-      if (nums[i] <= capacity) {
-        ++stolenHouses;
-        ++i;
-      }
-    return stolenHouses;
-  }
+        int ans = -1;
+        while(low <= high) {
+            int mid = (low + high) / 2;
+            if(isPossible(arr, k, mid)) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
 };
