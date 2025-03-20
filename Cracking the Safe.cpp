11ff1a3aac60d636b125b59abfd36e7960d72852
Arnab Biswas <arnab.biswas.gdsc@gmail.com>
2025-03-20 10:19:12 +0530

    Update Cracking the Safe.cpp

--- Cracking the Safe.cpp
+++ Cracking the Safe.cpp
@@ -1,32 +1,16 @@
 class Solution {
- public:
-  string crackSafe(int n, int k) {
-    string ans(n, '0');
-    dfs(pow(k, n), n, k, {ans}, ans);
-    return ans;
-  }
-
- private:
-  bool dfs(int passwordSize, int n, int k, unordered_set<string>&& seen,
-           string& path) {
-    if (seen.size() == passwordSize)
-      return true;
-
-    string prefix = path.substr(path.length() - n + 1);
-
-    for (char c = '0'; c < '0' + k; ++c) {
-      prefix.push_back(c);
-      if (!seen.contains(prefix)) {
-        seen.insert(prefix);
-        path.push_back(c);
-        if (dfs(passwordSize, n, k, std::move(seen), path))
-          return true;
-        path.pop_back();
-        seen.erase(prefix);
-      }
-      prefix.pop_back();
+public:
+    // Hamiltonian path solution using 0 as first digit for each prefix
+    string crackSafe(int n, int k) {
+        unordered_map<string, int> m;
+        string ans = "";
+        for (int i = 0; i < n - 1; i++)
+            ans += '0';
+        for (int i = 0; i < pow(k, n); i++) {
+            string temp = ans.substr(ans.size() - (n - 1));
+            m[temp] = (m[temp] + 1) % k;
+            ans += ('0' + m[temp]);
+        }
+        return ans;
     }
-
-    return false;
-  }
 };
