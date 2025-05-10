3473e8421397733514bc0ff8cd3d1a52d63a5a87
Arnab Biswas <arnab.biswas.gdsc@gmail.com>
2025-05-10 10:25:39 +0530

    Update Swim in Rising Water.cpp

--- Swim in Rising Water.cpp
+++ Swim in Rising Water.cpp
@@ -1,36 +1,34 @@
 class Solution {
- public:
-  int swimInWater(vector<vector<int>>& grid) {
-    constexpr int kDirs[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-    const int n = grid.size();
-    int ans = grid[0][0];
-    using T = tuple<int, int, int>;  // (grid[i][j], i, j)
-    priority_queue<T, vector<T>, greater<>> minHeap;
-    vector<vector<bool>> seen(n, vector<bool>(n));
+public:
+    int swimInWater(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int dx[5] = {-1, 0, 1, 0, -1};
 
-    minHeap.emplace(grid[0][0], 0, 0);
-    seen[0][0] = true;
+        vector<vector<int>> time(n, vector<int>(n, INT_MAX));
+        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<>> pq;
+        pq.push({grid[0][0],{0,0}});
+        time[0][0] = grid[0][0];
 
-    while (!minHeap.empty()) {
-      const auto [height, i, j] = minHeap.top();
-      minHeap.pop();
-      ans = max(ans, height);
-      if (i == n - 1 && j == n - 1)
-        break;
-      for (const auto& [dx, dy] : kDirs) {
-        const int x = i + dx;
-        const int y = j + dy;
-        if (x < 0 || x == n || y < 0 || y == n)
-          continue;
-        if (seen[x][y])
-          continue;
-        minHeap.emplace(grid[x][y], x, y);
-        seen[x][y] = true;
-      }
+        while(!pq.empty()){
+            auto [t, node] = pq.top();
+            auto [i, j] = node;
+            pq.pop();
+            if(i == n-1 && j == n-1){
+                break;
+            }
+            for(int k = 0; k < 4; k++){
+                int x = i+dx[k], y = j+dx[k+1];
+                if(x < 0 || x >= n || y < 0 || y >= n){
+                    continue;
+                }
+                int newTime = max(grid[x][y], t);
+                if(newTime < time[x][y]){
+                    time[x][y] = newTime;
+                    pq.push({newTime, {x, y}});
+                }
+            }
+        }
+
+        return time[n-1][n-1];
     }
-
-    return ans;
-  }
 };
-
-
