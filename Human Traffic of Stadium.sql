d21b8718cbc5f4321d496009f507423dfb5461f6
Arnab Biswas <arnab.biswas.gdsc@gmail.com>
2025-05-12 09:50:31 +0530

    Create Human Traffic of Stadium.sql

---
+++ Human Traffic of Stadium.sql
@@ -0,0 +1,12 @@
+with cte as 
+(Select *, id - row_number() over(order by id) as diff
+from Stadium 
+where people >= 100 )
+
+select id, visit_date, people  
+from cte
+where diff in 
+(select diff  
+from cte 
+group by diff
+having count(*) >=3)
