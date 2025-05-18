896ecf66d2a4a6d679f57a4b8497beb10ef0197a
Arnab Biswas <174271154+bytendpixel@users.noreply.github.com>
2025-05-18 10:47:20 +0530

    Create Numbers With Repeated Digits.cpp

---
+++ Numbers With Repeated Digits.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numDupDigitsAtMostN(int n) {
+       std::vector<int> digits{}; int temp = n + 1;
+        while (temp>0) {
+            digits.emplace_back(temp % 10);
+            temp/=10;
+        }
+        int ans = 0; int len = digits.size(); int curr = 9;
+        for (int i = 0; i < len - 1; i++) {
+            ans += curr;
+            curr *= 9 - i;
+        }
+        curr /= 9;
+        vector<bool> seen(10, false);
+        for (int i = 0; i < len; i++) {
+            int d = digits[len - i - 1];
+            for (int j = i == 0 ? 1 : 0; j < d; j++) if (!seen[j]) ans += curr;
+            curr /= 9 - i;
+            if (seen[d]) break;
+            seen[d] = true;
+        }
+        return n - ans;     
+    }
+};
