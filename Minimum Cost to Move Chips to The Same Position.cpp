bfdc90e6b18493fd441c7fe3eac70e49f112f740
Arnab Biswas <174271154+bytendpixel@users.noreply.github.com>
2025-06-01 11:06:53 +0530

    Create Minimum Cost to Move Chips to The Same Position.cpp

---
+++ Minimum Cost to Move Chips to The Same Position.cpp
@@ -0,0 +1,11 @@
+class Solution {
+ public:
+  int minCostToMoveChips(vector<int>& position) {
+    vector<int> count(2);
+    for (const int p : position)
+      ++count[p % 2];
+    return min(count[0], count[1]);
+  }
+};
+
+
